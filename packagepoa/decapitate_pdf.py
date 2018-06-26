e9b790e772ed8ba3a3e6329d22012edc505fd931
Graham Nott <gnott@starglobal.ca>
2018-06-26 10:53:26 -0700

    Linting decapitate_pdf.py with a small fix for the executable when running directly.

--- packagepoa/decapitate_pdf.py
+++ packagepoa/decapitate_pdf.py
@@ -4,15 +4,15 @@ import subprocess
 
 FORMAT = logging.Formatter("%(created)f - %(levelname)s - %(processName)s - %(name)s - %(message)s")
 LOGFILE = "decapitate_pdf.log"
+LOGGER = logging.getLogger(__file__)
+LOGGER.setLevel(logging.DEBUG)
+HDLR = logging.FileHandler(LOGFILE)
+HDLR.setLevel(logging.INFO)
+HDLR.setFormatter(FORMAT)
+LOGGER.addHandler(HDLR)
 
-logger = logging.getLogger(__file__)
-logger.setLevel(logging.DEBUG)
+PDF_EXECUTABLE_DEFAULT = "/opt/strip-coverletter/strip-coverletter-docker.sh"
 
-h = logging.FileHandler(LOGFILE)
-h.setLevel(logging.INFO)
-h.setFormatter(FORMAT)
-
-logger.addHandler(h)
 
 def decapitate_pdf_with_error_check(pdf_in, pdf_out_dir, poa_config=None):
     # configuration
@@ -25,31 +25,35 @@ def decapitate_pdf_with_error_check(pdf_in, pdf_out_dir, poa_config=None):
     # PDF out file name
     pdf_out = pdf_out_dir + pdf_in.split('/')[-1]
 
-    f = subprocess.Popen([pdf_executable, pdf_in, pdf_out], \
-                         stdout=subprocess.PIPE, \
-                         stderr=subprocess.PIPE)
+    process = subprocess.Popen(
+        [pdf_executable, pdf_in, pdf_out],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
 
-    stderr = f.stderr.read()
-    stdout = f.stdout.read()
+    stderr = process.stderr.read()
+    stdout = process.stdout.read()
 
     # subprocess.Popen doesn't interleave pipe output,
     # so neither will these log messages
-    map(logger.info, stdout.splitlines())
-    map(logger.error, stderr.splitlines())
+    map(LOGGER.info, stdout.splitlines())
+    map(LOGGER.error, stderr.splitlines())
 
-    return stderr != '' # no errors
+    return stderr != ''  # no errors
+
 
 if __name__ == '__main__':
     import sys
-    args = sys.argv[1:]
-    if len(args) < 2:
+    ARGS = sys.argv[1:]
+    if len(ARGS) < 2:
         print('Usage: decapitate_pdf.py <pdf-in> <pdf-out>')
         exit(1)
 
-    h2 = logging.StreamHandler()
-    h2.setLevel(logging.DEBUG)
-    h2.setFormatter(FORMAT)
-    logger.addHandler(h2)
+    HANDLER = logging.StreamHandler()
+    HANDLER.setLevel(logging.DEBUG)
+    HANDLER.setFormatter(FORMAT)
+    LOGGER.addHandler(HANDLER)
 
-    pin, pout = args[:2]
-    decapitate_pdf_with_error_check(pin, pout)
+    PIN = ARGS[0]
+    POUT = ARGS[1]
+    CONFIG = {'strip_coverletter_executable': PDF_EXECUTABLE_DEFAULT}
+    decapitate_pdf_with_error_check(PIN, POUT, CONFIG)
